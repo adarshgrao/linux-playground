2b5d1d3bb0a901ceba5730b39a576a91b5c17894
Adarsh <adarshg.rao@gmail.com>
2016-08-25 01:00:59 +0530

    test.sh

---
+++ test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+date 
+
+#date -d
+
+date -f
+date -j
+date -n
+date -r
+date -t
+date -u
+
+date -v
+
+date "+DATE: %Y-%m-%d%nTIME: %H:%M:%S"
+
+date -v1m -v+1y
+
+date -v1d -v3m -v0y -v-1d
+
+
+
+
+
+
